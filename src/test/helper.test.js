725772159e24b9b90ad0413a94195363ff7a5841
saruj <jomsaruj@sarujs-MacBook-Pro-2.local>
2021-10-21 21:16:50 +0700

    Add test cases description

--- src/test/helper.test.js
+++ src/test/helper.test.js
@@ -74,15 +74,30 @@ beforeEach(() => {
     ]
 
 });
-  
+
+/**
+ * Test Case ID: 1
+ * Given a user who was born on 10 Oct 2000
+ * Function calcAge must return 21 years old
+ */
 test("calulate user's age of user who was born on 10 Oct 2000", () => {
     expect(helper.calcAge("10-10-2000")).toBe(21);
 });
   
+/**
+ * Test Case ID: 2
+ * Given a user who was born on 31 Oct 2000
+ * Function calcAge must return 20 years old
+ */
 test("calulate user's age of user who was born on 31 Dec 2000", () => {
     expect(helper.calcAge("31-12-2000")).toBe(20);
 });
 
+/**
+ * Test Case ID: 3
+ * Given a user who was born today on year 2000
+ * Function calcAge must return 21 years old
+ */
 test("calulate user's age of user who was born today on year 2000", () => {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
@@ -92,54 +107,119 @@ test("calulate user's age of user who was born today on year 2000", () => {
     expect(helper.calcAge(today)).toBe(21);
 });
 
+/**
+ * Test Case ID: 4
+ * Given a user who was born on 1 Jan this year
+ * Function calcAge must return 0 years old
+ */
 test("calulate user's age of user who was born on 1 Jan this year", () => {
     expect(helper.calcAge("1-1-2021")).toBe(0);
 });
 
+/**
+ * Test Case ID: 5
+ * Given a user who was born on 31 Dec next year
+ * Function calcAge must throw error with massage 'you are using invalid date'
+ */
 test("calulate user's age of user who was born in the future", () => {
     expect(() => helper.calcAge("31-12-2022")).toThrow('you are using invalid date');
 });
  
+/**
+ * Test Case ID: 6
+ * Given a date with '0' in front
+ * Function calcAge must return 21'
+ */
 test("calulate user's age with date 01 (have zero in front)", () => {
     expect(helper.calcAge("01-01-2000")).toBe(21);
 });
 
+/**
+ * Test Case ID: 7
+ * Given a date with month 'July'
+ * Function calcAge must throw error with massage 'you are using wrong date format'
+ */
 test("calulate user's age of user who was born in 'July' ", () => {
     expect(() => helper.calcAge("1-July-2000")).toThrow('you are using wrong date format');
 });
 
+/**
+ * Test Case ID: 8
+ * Given a empty string
+ * Function calcAge must throw error with massage 'you are using wrong date format'
+ */
 test("calulate user's age with empty string", () => {
     expect(() => helper.calcAge("")).toThrow('you are using wrong date format');
 });
 
+/**
+ * Test Case ID: 9
+ * Given a date with yyyy-mm-dd
+ * Function calcAge must throw error with massage 'you are using wrong date format'
+ */
 test("calulate user's age with format yyyy-mm-dd", () => {
     expect(() => helper.calcAge("2000-12-2")).toThrow('you are using wrong date format');
 });
 
+/**
+ * Test Case ID: 10
+ * Given a date with day 0
+ * Function calcAge must throw error with massage 'you are using invalid date'
+ */
 test("calulate user's age of user who was born on day 0", () => {
     expect(() => helper.calcAge("0-1-2000")).toThrow('you are using invalid date');
 });
 
+/**
+ * Test Case ID: 11
+ * Given a date with day -1
+ * Function calcAge must throw error with massage 'you are using invalid date'
+ */
 test("calulate user's age of user who was born on day -1, test นี้แก้ได้นะ", () => {
     expect(() => helper.calcAge("-1-01-2000")).toThrow('you are using invalid date');
 });
 
+/**
+ * Test Case ID: 12
+ * Given a date without '-'
+ * Function calcAge must throw error with massage 'you are using wrong date format'
+ */
 test("calulate user's age with no '-' format", () => {
     expect(() => helper.calcAge("01012000")).toThrow('you are using wrong date format');
 });
 
+/**
+ * Test Case ID: 13
+ * Given a date with '/'
+ * Function calcAge must throw error with massage 'you are using wrong date format'
+ */
 test("calulate user's age with '/' format", () => {
     expect(() => helper.calcAge("1/1/2000")).toThrow('you are using wrong date format');
 });
 
+/**
+ * Test Case ID: 14
+ * Given a date with month 13
+ * Function calcAge must throw error with massage 'you are using invalid date'
+ */
 test("calulate user's age with invalid month", () => {
     expect(() => helper.calcAge("1-13-2000")).toThrow('you are using invalid date');
 });
 
+/**
+ * Test Case ID: 15
+ * Given a date with day 32
+ * Function calcAge must throw error with massage 'you are using invalid date'
+ */
 test("calulate user's age with invalid date", () => {
     expect(() => helper.calcAge("32-1-2000")).toThrow('you are using invalid date');
 });
 
+/**
+ * Test Case ID: 16
+ * Given a data format that send by GOV team (array)
+ * Function convertGovJson must generate JSON object from the array
+ */
 test("check JSON have correct key&value", () => {
     let normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     let expected = {reservation_id: 1};
@@ -153,6 +233,11 @@ test("check JSON have correct key&value", () => {
     expect(normal_young_user_json).toEqual(expected);
 });
 
+/**
+ * Test Case ID: 17
+ * Given an invalid data (in case GOV team accidentally send incorrect format)
+ * Function convertGovJson must still generate JSON object that contain all important key
+ */
 test("check JSON have all require field(eventhough value is undefine)", () => {
     const invalid_user_json = helper.convertGovJson(invalid_user[0]);
     expect(invalid_user_json.reservation_id).toEqual(expect.any(Number))
@@ -165,67 +250,132 @@ test("check JSON have all require field(eventhough value is undefine)", () => {
     expect(invalid_user_json.address).toEqual(expect.any(String))
 });
 
+/**
+ * Test Case ID: 18
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key reservation_id has value 1
+ */
 test("check JSON has valid reservation_id format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.reservation_id).toBe(1);
 });
 
+/**
+ * Test Case ID: 19
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key timestamp has value 2021-10-20T15:19:56.609000
+ */
 test("check JSON has valid timestamp format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.register_timestamp).toEqual("2021-10-20T15:19:56.609000");
 });
 
+/**
+ * Test Case ID: 20
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key name has value 'Joel'
+ */
 test("check JSON has valid name format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.name).toEqual("Joel");
 });
 
+/**
+ * Test Case ID: 21
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key surename has value 'Miller'
+ */
 test("check JSON has valid surname format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.surname).toEqual("Miller");
 });
 
+/**
+ * Test Case ID: 22
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key birth_date has value "20-10-2000"
+ */
 test("check JSON has valid date format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.birth_date).toEqual("20-10-2000");
 });
 
+/**
+ * Test Case ID: 23
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key citizen_id has value "1234567890123"
+ */
 test("check JSON has valid id format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.citizen_id).toEqual("1234567890123");
 });
 
+/**
+ * Test Case ID: 24
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key occupation has value "Teacher"
+ */
 test("check JSON has valid occupation format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.occupation).toEqual("Teacher");
 });
 
+/**
+ * Test Case ID: 25
+ * Given normal_young_user (see detail above)
+ * Function convertGovJson must generate JSON object with key address has value "Kasetsart University"
+ */
 test("check JSON has valid address format", () => {
     const normal_young_user_json = helper.convertGovJson(normal_young_user[0]);
     expect(normal_young_user_json.address).toEqual("Kasetsart University");
 });
 
+/**
+ * Test Case ID: 26
+ * Given date with format "10-20-2000" 
+ * Function toSlashDate must return "2000/20/10"
+ */
 test("check modify normal date format", () => {
     let date = "10-20-2000" 
     expect(helper.toSlashDate(date)).toEqual("2000/20/10");
 });
 
+/**
+ * Test Case ID: 27
+ * Given date with format "01234-12233-200000000" 
+ * Function toSlashDate must return "200000000/12233/01234"
+ */
 test("check modify invalid date format", () => {
     let date = "01234-12233-200000000" 
     expect(helper.toSlashDate(date)).toEqual("200000000/12233/01234");
 });
 
+/**
+ * Test Case ID: 28
+ * Given 2 users 
+ * Function modifyPeopleList must return array that contain 2 JSON object
+ */
 test("check number of element in list", () => {
     let newList = helper.modifyPeopleList(listOfUser)
     expect(newList.length).toBe(2)
 });
 
+/**
+ * Test Case ID: 29
+ * Given listOfUser (see above)
+ * Function modifyPeopleList must return array that contain 2 JSON object with queue order 
+ */
 test("check user order", () => {
     let newList = helper.modifyPeopleList(listOfUser)
     expect(newList[0].reservation_id).toEqual(1)
     expect(newList[1].reservation_id).toEqual(2)
 });
 
+/**
+ * Test Case ID: 30
+ * Given young normal user (non doctor or nurse)
+ * Function setPriorityPerson must return priority level 3
+ */
 test("set priority for normal young user", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -238,6 +388,11 @@ test("set priority for normal young user", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("3");
 });
 
+/**
+ * Test Case ID: 31
+ * Given old normal user(older than 60) (non doctor or nurse)
+ * Function setPriorityPerson must return priority level 2
+ */
 test("set priority for normal old user", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -250,6 +405,11 @@ test("set priority for normal old user", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("2");
 });
 
+/**
+ * Test Case ID: 32
+ * Given young user which have occupation = Nurse (with caps N)
+ * Function setPriorityPerson must return priority level 1
+ */
 test("set priority for young Nurse (Nurse with caps 'N')", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -262,6 +422,11 @@ test("set priority for young Nurse (Nurse with caps 'N')", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("1");
 });
 
+/**
+ * Test Case ID: 33
+ * Given young user which have occupation = nurse
+ * Function setPriorityPerson must return priority level 1
+ */
 test("set priority for young nurse", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -274,6 +439,11 @@ test("set priority for young nurse", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("1");
 });
 
+/**
+ * Test Case ID: 34
+ * Given young user which have occupation = Doctor(with caps D)
+ * Function setPriorityPerson must return priority level 1
+ */
 test("set priority for young Doctor(Doctor with 'D')", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -286,6 +456,11 @@ test("set priority for young Doctor(Doctor with 'D')", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("1");
 });
 
+/**
+ * Test Case ID: 35
+ * Given young user which have occupation = doctor
+ * Function setPriorityPerson must return priority level 1
+ */
 test("set priority for young doctor", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -298,6 +473,11 @@ test("set priority for young doctor", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("1");
 });
 
+/**
+ * Test Case ID: 36
+ * Given old user which have occupation = doctor and older than 60
+ * Function setPriorityPerson must return priority level 1
+ */
 test("set priority for old doctor", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -310,6 +490,11 @@ test("set priority for old doctor", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("1");
 });
 
+/**
+ * Test Case ID: 37
+ * Given old user which have occupation = nurse and older than 60
+ * Function setPriorityPerson must return priority level 1
+ */
 test("set priority for young nurse", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -322,6 +507,11 @@ test("set priority for young nurse", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("1");
 });
 
+/**
+ * Test Case ID: 38
+ * Given normal user with exact 60 year old
+ * Function setPriorityPerson must return priority level 3
+ */
 test("set priority for exact 60 user", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -334,6 +524,11 @@ test("set priority for exact 60 user", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("3");
 });
 
+/**
+ * Test Case ID: 39
+ * Given normal user with exact 61 year old
+ * Function setPriorityPerson must return priority level 3
+ */
 test("set priority for exact 61 user", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
@@ -346,6 +541,11 @@ test("set priority for exact 61 user", () => {
     expect(helper.setPriorityPerson(normal_young_user)).toEqual("2");
 });
 
+/**
+ * Test Case ID: 40
+ * Given normal user with exact 59 year old
+ * Function setPriorityPerson must return priority level 3
+ */
 test("set priority for exact 59 user", () => {
     const normal_young_user = {reservation_id: 1};
     normal_young_user['register_timestamp'] = "2021-10-20T15:19:56.609000";
