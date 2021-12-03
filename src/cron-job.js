bdeda9d2fd94018c37c487dbf92136656a82f1a9
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2021-12-03 13:39:02 +0700

    Remove unused logs

--- src/cron-job.js
+++ src/cron-job.js
@@ -2,7 +2,8 @@ const cron = require("node-cron");
 const fetcher = require("./helpers/fetchDataGovToDb");
 
 let d = new Date();
-d.setDate(d.getDate() + 7);
+let day_ahead = process.env.FETCH_AHEAD;
+d.setDate(d.getDate() + day_ahead);
 let year = d.getFullYear();
 let day = d.getDay();
 let month = d.getMonth();
@@ -11,7 +12,6 @@ let hour = process.env.FETCH_HOUR;
 let minute = process.env.FETCH_MINUTE;
 
 const JOB_SCHEDULE = `${minute} ${hour} * * *`;
-console.log(JOB_SCHEDULE);
 if (day < 10) day = `0${day}`;
 if (month < 10) month = `0${month}`;
 let date = `${day}-${month}-${year}`;
