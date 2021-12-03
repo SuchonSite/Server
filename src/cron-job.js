b1f2579359f9cf20ef49ceeeb17ae9a19eec54d2
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2021-12-03 13:32:39 +0700

    Add cron job schedule

--- src/cron-job.js
+++ src/cron-job.js
@@ -1,18 +1,21 @@
-// const config = require('./config');
-// const { JOB_SCHEDULE } = config;
-// const cron = require('node-cron');
-// const fetcher = require('./helpers/fetchDataGovToDb')
+const cron = require("node-cron");
+const fetcher = require("./helpers/fetchDataGovToDb");
 
-// let d = new Date();
-// d.setDate(d.getDate() + 7);
-// let year = d.getFullYear();
-// let day = d.getDay();
-// let month = d.getMonth();
-// if (day < 10) day = `0${day}`
-// if (month < 10) month = `0${month}`
+let d = new Date();
+d.setDate(d.getDate() + 7);
+let year = d.getFullYear();
+let day = d.getDay();
+let month = d.getMonth();
 
-// let date = `${day}-${month}-${year}`
+let hour = process.env.FETCH_HOUR;
+let minute = process.env.FETCH_MINUTE;
 
-// cron.schedule(JOB_SCHEDULE, () => {
-//   fetcher.fetchDataGovToDb(date)
-// });
+const JOB_SCHEDULE = `${minute} ${hour} * * *`;
+console.log(JOB_SCHEDULE);
+if (day < 10) day = `0${day}`;
+if (month < 10) month = `0${month}`;
+let date = `${day}-${month}-${year}`;
+
+cron.schedule(JOB_SCHEDULE, () => {
+  fetcher.fetchDataGovToDb(date);
+});
